d7119de7aee7c915122b2b8baed5ce7338c1cc03
Alex Ivanov <alex@meticulous.ai>
2024-10-08 09:57:40 +0100

    update data 8/10

--- components/flight-search/page.tsx
+++ components/flight-search/page.tsx
@@ -88,7 +88,7 @@ const FlightSearchPage: FunctionComponent<{ airports: Airport[] }> = ({
             </div>
 
             <div className="font-bold">
-              Last availability data update: 07/07/2024 ~08:00 CET
+              Last availability data update: 08/07/2024 ~08:00 CET
             </div>
 
             <div className="mt-3 font-bold">
