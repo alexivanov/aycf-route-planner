303181c67cef9c1d879eadba53ea8f7c3f96552f
Alex Ivanov <alex@meticulous.ai>
2024-10-14 09:12:10 +0100

    move to fetching within the page

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,25 @@ import { loadFlightData } from "@/lib/load-flight-data";
 import { Airport } from "@/lib/types";
 import FlightSearchPage from "@/components/flight-search/page";
 
-const flights = await loadFlightData();
+const Page = async () => {
+  const flights = await loadFlightData();
 
-const latestDataUpdateTime = flights
-  ? new Date(Math.max(...flights.map((flight) => flight.createdAt.getTime())))
-  : null;
+  const latestDataUpdateTime = flights
+    ? new Date(Math.max(...flights.map((flight) => flight.createdAt.getTime())))
+    : null;
 
-const allAirports = flights
-  .reduce<Airport[]>((acc, flight) => {
-    if (!acc.find((airport) => airport.code === flight.from.code)) {
-      acc.push(flight.from);
-    }
-    if (!acc.find((airport) => airport.code === flight.to.code)) {
-      acc.push(flight.to);
-    }
-    return acc;
-  }, [])
-  .sort((a, b) => a.code.localeCompare(b.code));
+  const allAirports = flights
+    .reduce<Airport[]>((acc, flight) => {
+      if (!acc.find((airport) => airport.code === flight.from.code)) {
+        acc.push(flight.from);
+      }
+      if (!acc.find((airport) => airport.code === flight.to.code)) {
+        acc.push(flight.to);
+      }
+      return acc;
+    }, [])
+    .sort((a, b) => a.code.localeCompare(b.code));
 
-const Page = () => {
   return (
     <FlightSearchPage
       airports={allAirports}
--- app/return-journey-finder/page.tsx
+++ app/return-journey-finder/page.tsx
@@ -2,21 +2,20 @@ import { loadFlightData } from "@/lib/load-flight-data";
 import { Airport } from "@/lib/types";
 import { ReturnJourneyFinder } from "@/components/return-journey-finder/page";
 
-const flights = await loadFlightData();
+const Page = async () => {
+  const flights = await loadFlightData();
 
-const allAirports = flights
-  .reduce<Airport[]>((acc, flight) => {
-    if (!acc.find((airport) => airport.code === flight.from.code)) {
-      acc.push(flight.from);
-    }
-    if (!acc.find((airport) => airport.code === flight.to.code)) {
-      acc.push(flight.to);
-    }
-    return acc;
-  }, [])
-  .sort((a, b) => a.code.localeCompare(b.code));
-
-const Page = () => {
+  const allAirports = flights
+    .reduce<Airport[]>((acc, flight) => {
+      if (!acc.find((airport) => airport.code === flight.from.code)) {
+        acc.push(flight.from);
+      }
+      if (!acc.find((airport) => airport.code === flight.to.code)) {
+        acc.push(flight.to);
+      }
+      return acc;
+    }, [])
+    .sort((a, b) => a.code.localeCompare(b.code));
   return <ReturnJourneyFinder airports={allAirports} />;
 };
 
