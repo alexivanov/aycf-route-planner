0d594b10f851676dc81dc0875916b58bf95a96ca
Alex Ivanov <alex@meticulous.ai>
2024-12-30 10:33:39 +0000

    force dynamic on route endpoint

--- app/api/raw-data/route.ts
+++ app/api/raw-data/route.ts
@@ -1,6 +1,8 @@
 import { loadFlightData } from "@/lib/data/load-flight-data";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const flights = await loadFlightData();
 
